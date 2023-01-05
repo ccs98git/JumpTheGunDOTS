fc7163e8b6c0855c0bd0553827cee9332ee4f78e
Claudia Castelo <clca@itu.dk>
2023-01-05 17:50:51 +0100

    jobs

--- Assets/Scripts/Systems/CannonBallMovementSystem.cs
+++ Assets/Scripts/Systems/CannonBallMovementSystem.cs
@@ -37,6 +37,7 @@ public partial struct CannonBallMovementSystem : ISystem
 
     }
 }
+[WithAll(typeof(CannonBall))]
 [BurstCompile]
 partial struct CannonBallJob : IJobEntity
 {
@@ -70,18 +71,81 @@ public partial struct DestroyCannonBalls : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        //state.EntityManager.
         var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
         var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
-        
-        foreach (var (transPos, timerLife, entity) in SystemAPI.Query<CannonBall, RefRO<TimePar>>().WithAll<CannonBall>().WithEntityAccess())
+        foreach (var (transPos, par, timerLife, entity) in SystemAPI.Query<CannonBall, RefRO<Parabola>, RefRO<TimePar>>().WithAll<CannonBall>().WithEntityAccess())
         {
             if (timerLife.ValueRO.parTime >= 1.0f)
             {
+                // Looks up ground tile and lowers it if it is above the minimum height
+                // Rounds to integer values -> meaning it rounds to whole numbers for ground identification
+                // through rough approximation.
+                (int, int) CB_pos = ((int)math.round(par.ValueRO.end.x), (int)math.round(par.ValueRO.end.y));
+
+                foreach (var (groundElement, groundEntity) in SystemAPI.Query<Ground>().WithEntityAccess())
+                {
+                    if (groundElement.xPos == CB_pos.Item1 && groundElement.yPos == CB_pos.Item2)
+                    {
+                        // When the tile is found -> check height index. If above 1
+                        // Lower by 1 and relocate the object so it reduces its height by 1
+                        if (groundElement.height > 1)
+                        {
+
+                            int newheight = groundElement.height - 1;
+                            int newColorForGround = groundElement.color + 8;
+                            // update color of ground
+                            Vector3 flatColor = new Vector3(newColorForGround, 202f, 56f).normalized; //<- not sure if/why this works, or if it works as intended.
+                            Color newColor = UnityEngine.Color.HSVToRGB(flatColor.x, flatColor.y, flatColor.z);
+
+                            ecb.SetComponent(groundEntity, new URPMaterialPropertyBaseColor
+                            {
+                                Value = (UnityEngine.Vector4)newColor
+                            });
+
+                            // Relocate and resize the ground element.
+                            float heightIndex = 0.2f * (newheight); // <- random int between 1 and maxHeight (inclusive)
+
+                            // Transform Manipulation, place and scale.
+                            float3 Position = new float3(groundElement.xPos, (heightIndex / 2.0f), groundElement.yPos);
+                            float3 newScale = new float3(1, 1 + heightIndex, 1);
+
+                            ecb.SetComponent(groundEntity, new Translation
+                            {
+                                Value = Position
+                            });
+                            ecb.AddComponent(groundEntity, new NonUniformScale
+                            {
+                                Value = newScale
+                            });
+
+
+                            // update last
+                            ecb.SetComponent(groundEntity, new Ground
+                            {
+                                height = newheight,
+                                color = newColorForGround,
+                                xPos = groundElement.xPos,
+                                yPos = groundElement.yPos,
+                                hasCannon = groundElement.hasCannon
+                            });
+
+
+
+                            break; // <- since each cannonball only has one desitnation, we can break
+                            // the loop and carry on at this point.
+                        }
+                        else
+                        {
+                            break; // found tile, no edits needed. Break loop since same logic as above.
+                        }
+
+                    }
+                }
+
+                // Destroys Cannonball Entity - an evnetuality
                 ecb.DestroyEntity(entity);
             }
         }
-        //ecb.Dispose();
 
 
     }
--- Assets/Scripts/Systems/CannonShootingSystem.cs
+++ Assets/Scripts/Systems/CannonShootingSystem.cs
@@ -3,6 +3,8 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Unity.VisualScripting.FullSerializer;
+using UnityEditor.Compilation;
 using UnityEngine;
 using UnityEngine.TerrainUtils;
 [BurstCompile]
@@ -54,8 +56,54 @@ public partial struct CannonShootingSystem : ISystem
                 ecb.SetComponent(cannonBallEntity, parabolaData);
             }
         }
-       
+        /*
+        var cannonShootlJob = new CannonShootJob
+        {
+            time = SystemAPI.Time.DeltaTime,
+            random = UnityEngine.Random.Range(1, config.ValueRO.cannonCooldown),
+            ecb = ecb
+        };
+        state.Dependency = cannonShootlJob.Schedule(state.Dependency);
+        state.Dependency.Complete();
+        //var handle = cannonShootlJob.ScheduleParallel(state.Dependency);
+        //handle.Complete();
         
+        */
     }
 }
+/*
+[WithAll(typeof(Cannon))]
+[BurstCompile]
+partial struct CannonShootJob : IJobEntity
+{
+    public float time;
+    public float random;
+    public EntityCommandBuffer ecb;
+    void Execute(ref Cannon cannon, ref CannonAspect translation, ref BallAspect playerAspect, ref Config config)
+    {
+        cannon.coolDown += time;
+        if (cannon.coolDown > config.cannonCooldown)
+        {
+            cannon.coolDown = random;
+            var endPoint = playerAspect.transform;
+            var startPoint = translation.transform.Position;
+            var distance = new Vector2(endPoint.Position.z - startPoint.z, endPoint.Position.x + startPoint.x).magnitude;
+            var duration = distance / 5.0f;
+            if (duration < 0.0001)
+            {
+                duration = 1;
+            }
 
+            var cannonBallEntity = ecb.Instantiate(cannon.CannonBall);
+            ecb.SetComponent(cannonBallEntity, new Translation
+            {
+                Value = translation.transform.Position
+            });
+            Parabola parabolaData = ParabolaSolve.Create(startPoint.y, config.maxHeight, endPoint.Position.y);
+            parabolaData.start = startPoint.xz;
+            parabolaData.end = endPoint.Position.xz;
+            parabolaData.duration = duration;
+            ecb.SetComponent(cannonBallEntity, parabolaData);
+        }
+    }
+}*/
